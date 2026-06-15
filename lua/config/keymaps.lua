67acee72c3f5b0a19a58a17a4f80e4447a49775c
Lou Varndell <lou.varndell@gmail.com>
2026-06-15 09:18:56 -0400

    init

--- lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>lz", vim.cmd.Lazy)
-
--- lua/config/options.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.o.clipboard = 'unnamedplus'
-vim.o.hlsearch = false
-vim.o.shiftwidth = 4
-
